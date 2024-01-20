55657ca3b2e06d70a836bf6cb0f13243703bf340
Supratim Mondal <supratimmondal50@gmail.com>
2024-01-20 21:49:09 +0530

    configure dotenv and conect with database

--- src/app.js
+++ src/app.js
@@ -12,4 +12,6 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({limit: "16kb"}));
-app.use()
+app.use(express.static("public"));
+
+export {app}
--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants";
+import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
     try {
+        console.log(process.env.MONGODB_URI);
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log("DB connection successful");
     } catch (error) {
